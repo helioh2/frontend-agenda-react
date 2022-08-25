9388f6344d7099f425601733c0fa2115f1ae2673
Helio <heliohenrique3@gmail.com>
2022-08-25 15:17:04 -0300

    Criação do projeto de frontend e testes com react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import imagemJuca from './13.gif'
 
-function App() {
+export function Car(props) {
+  return <h2 style={{color:props.color}}>I am a {props.color} Car! Meu nome é {props.nome}</h2>;
+}
+
+export function Contador(props) {
+
+  const [contador, setContador] = useState(0)
+
+  function aumentaContador(){
+    setContador(contador + 1)
+  }
+
+  return (
+    <>
+    {contador}
+    <button onClick={aumentaContador}>Aumenta</button>
+    </>
+  )
+
+}
+
+
+function App() {  // componente
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ola!!!
         </p>
+        <img src={imagemJuca} alt="IMAGEMJUCA"/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +42,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <Car color="red" nome="Relampago Marquinhos"/>
+
+        <Contador />
       </header>
     </div>
   );
